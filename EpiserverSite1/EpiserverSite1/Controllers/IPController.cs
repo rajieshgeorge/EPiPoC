735c99ac445009c975f63f9a237a23868e536de7
Niklas Wanngren <niklas.wanngren@ffcg.se>
2017-09-12 13:34:15 +0200

    init

---
+++ EpiserverSite1/EpiserverSite1/Controllers/IPController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EpiserverSite1.Controllers
+{
+    public class IPController : Controller
+    {
+        // GET: IP
+        public ActionResult Index()
+        {
+   
+                    var str = "";
+                    foreach (System.Net.IPAddress address in System.Net.Dns.GetHostEntry(System.Net.Dns.GetHostName()).AddressList)
+                    {
+                        str += address.ToString() + "-" + System.Net.IPAddress.IsLoopback(address).ToString() + "</br>";
+                    }
+                    str += "</br> </br> and Mac  </br> </br>";
+
+                    foreach (var net in NetworkInterface.GetAllNetworkInterfaces().Where(nic => nic.OperationalStatus == OperationalStatus.Up).Select(nic => nic.GetPhysicalAddress().ToString()))
+                    {
+                        str += net + "</br>";
+                    }
+                    return new ContentResult() { Content = str };
+                    }
+                    
+                    
+
+    }
+}
---
+++ EpiserverSite1/EpiserverSite1/Global.asax.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace EpiserverSite1
+{
+    public class EPiServerApplication : EPiServer.Global
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+
+            //Tip: Want to call the EPiServer API on startup? Add an initialization module instead (Add -> New Item.. -> EPiServer -> Initialization Module)
+
+            RouteTable.Routes.MapRoute(
+                "Ip",
+                "ip/{action}",
+                new
+                {
+                    controller = "Ip",
+                    action = "Index"
+                },
+                new[] { "EpiserverSite1.Controllers" }
+            );
+        }
+    }
+}
